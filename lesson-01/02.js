a52798487b5f280b554f9006c951b06ad98a33a6
MempfisD <komrad.nda@yandex.ru>
2023-12-10 23:54:04 +0300

    fix_1

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -4,5 +4,5 @@
 
 let number1 = Math.floor(Math.random() * 100);
 console.log(
-  number1 + " " + ":Это четное число" || number2 + " " + ":Это нечетное число"
+  number1 + " " + ":Это четное число" || number1 + " " + ":Это нечетное число"
 );
