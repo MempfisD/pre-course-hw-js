33a1e8eabc437b6ca3bb5d0ae0af290284aa83aa
MempfisD <komrad.nda@yandex.ru>
2023-12-15 00:20:57 +0300

    Add lesson_04

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -7,7 +7,7 @@
 
 let number = 1;
 
-console.log(number++);
-console.log(++number);
-console.log(number--);
-console.log(--number);
+console.log(number++); // 1
+console.log(++number); // 3
+console.log(number--); // 3
+console.log(--number); // 1
