b3a4fb37c99349fba2f076dcf82d924c9946380a
MempfisD <komrad.nda@yandex.ru>
2023-12-15 15:29:27 +0300

    add fix lesson_5.4

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -13,7 +13,7 @@ switch (true) {
 		console.log("У меня 100 пудов все получится");
 		break
 	case rule.length === 25:
-		console.log("«50 на 50»");
+		console.log("50 на 50");
 		break
 	default:
 		console.log("Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.");
