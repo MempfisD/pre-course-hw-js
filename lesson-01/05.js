f4628eeb284cf1fe1fd218a4851f012e05c1fd3c
MempfisD <komrad.nda@yandex.ru>
2023-12-15 16:16:12 +0300

    add fix lesson_5.9

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -4,19 +4,18 @@
 // и если больше 25 "У меня 100 пудов все получится", а если ровно 25, то «50 на 50»
 
 let rule = 'Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.';
-console.log(rule.length);
 
 switch (true) {
-	case (rule.length < 25):
+	case rule.length < 25:
 		console.log('Все таки нет правил без исключения');
 		break;
-	case (rule.length > 25):
+	case rule.length > 25:
 		console.log('У меня 100 пудов все получится');
 		break;
-	case (rule.length === 25):
+	case rule.length === 25:
 		console.log('50 на 50');
 		break;
 	default:
-		console.log('Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.');
+		console.log('Нет правил без исключения');
 		break;
 }
