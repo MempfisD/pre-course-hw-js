4b52f60591d841e939adf5deb154b1ffaf6e11f9
MempfisD <komrad.nda@yandex.ru>
2023-12-27 23:40:50 +0300

    fix_1

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,10 +1,3 @@
-// Переделай функцию на стрелочный синтаксис
-
-// function doArrow(value) {
-// 	console.log(value);
-// }
-// doArrow('Передай 5');
-
 const doArrow = value => {
 	console.log(value);
 };
