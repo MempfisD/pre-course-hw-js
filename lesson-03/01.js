0d95dc6bba72aebe7f865238be6ea9e24900f7fe
Dmitry Naumov <komrad.nda@yandex.ru>
2024-01-16 23:47:49 +0300

    add less-03

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,10 @@
-// code
+const colors = ['синий', 'черный', 'зеленый', 'красный', 'желтый', 'зеленый'];
+
+function createColorString() {
+	const germanFlag = colors.filter(
+		color => color === 'черный' || color === 'красный' || color === 'желтый'
+	);
+	return germanFlag.join('-');
+}
+
+console.log(createColorString());
